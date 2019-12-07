d53de35b735824a67ffece172c23a6f83e5510d4
Dmitriy Virych <dmitriy.virych@l1inc.com>
2019-12-07 17:01:05 +0200

    Initial Commit

---
+++ Recipes_demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Recipes_demo
+//
+//  Created by Dima Virych on 07.12.2019.
+//  Copyright © 2019 Virych. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
